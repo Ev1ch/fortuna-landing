1997f4c2c671d6e2e6178f0221c038fb965fe306
Vlad Zusko <vladzvu11@gmail.com>
2021-10-02 17:52:57 +0300

    Added basic responsibility

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import Swiper, { Navigation } from 'swiper';
 Swiper.use([Navigation]);
 
+const Breakpoints = {
+    MD: 768,
+    LG: 1200,
+};
+
 const TeamSelectors = {
     CONTAINER: '#team-swiper',
     PREVIOUS_BUTTON: '.team-members__previous',
@@ -11,11 +16,25 @@ const TeamSelectors = {
 const AdvisoryTeamSelectors = {
     CONTAINER: '#advisory-team-swiper',
     PREVIOUS_BUTTON: '.team-members__previous',
+    MEMBER: 'team-member',
     NEXT_BUTTON: '.team-members__next',
 };
 
 const TEAM_SWIPER_CONFIG = {
-    slidesPerView: 'auto',
+    breakpoints: {
+        0: {
+            slidesPerGroup: 1,
+            slidesPerView: 1,
+        },
+        [Breakpoints.MD]: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+        },
+        [Breakpoints.LG]: {
+            slidesPerGroup: 4,
+            slidesPerView: 4,
+        },
+    },
     navigation: {
         prevEl: TeamSelectors.PREVIOUS_BUTTON,
         nextEl: TeamSelectors.NEXT_BUTTON,
@@ -26,11 +45,25 @@ const TEAM_SWIPER_CONFIG = {
 };
 
 const ADVISORY_TEAM_SWIPER_CONFIG = {
-    slidesPerView: 'auto',
+    breakpoints: {
+        0: {
+            slidesPerGroup: 1,
+            slidesPerView: 1,
+        },
+        [Breakpoints.MD]: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+        },
+        [Breakpoints.LG]: {
+            slidesPerGroup: 4,
+            slidesPerView: 4,
+        },
+    },
     navigation: {
         prevEl: AdvisoryTeamSelectors.PREVIOUS_BUTTON,
         nextEl: AdvisoryTeamSelectors.NEXT_BUTTON,
     },
+    slideClass: AdvisoryTeamSelectors.MEMBER,
     observer: true,
     observeParents: true,
 };
@@ -40,3 +73,23 @@ const advisoryTeamSwiper = new Swiper(
     AdvisoryTeamSelectors.CONTAINER,
     ADVISORY_TEAM_SWIPER_CONFIG,
 );
+
+const NavigationSelectors = {
+    CONTAINER: '.navigation',
+    TOGGLE_BUTTON: '.navigation__toggle',
+};
+
+const NavigationClasses = {
+    BURGER: 'navigation--burger',
+};
+
+const navigationEl = document.querySelector(NavigationSelectors.CONTAINER);
+const navigationToggleEl = document.querySelector(
+    NavigationSelectors.TOGGLE_BUTTON,
+);
+
+function toggleNavigation() {
+    navigationEl.classList.toggle(NavigationClasses.BURGER);
+}
+
+navigationToggleEl.addEventListener('click', toggleNavigation);
